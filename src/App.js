e9cbe62b24597423eae470d738325838efe4fdc9
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-08 16:08:09 +0100

    first integration test (events) added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { mockData } from './mock-data';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+
+  state = {
+    events: []
+  }
+
   render() {
     return (
       <div className="App">
         <CitySearch />
         <NumberOfEvents />
-        <EventList />
+        <EventList events={this.state.events}/>
       </div>
     );
   }
