31ad9672bba03b8f4cabbc29ca6c108a8b53738e
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-15 13:58:52 +0100

    Event hide show

--- src/Event.js
+++ src/Event.js
@@ -20,10 +20,12 @@ class Event extends Component {
 
   render() {
     const { event } = this.props;
+    const { collapsed } = this.state;
+    console.log('event state collapsed:', this.state.collapsed);
 
     return (
       <div className="event">
-        <div className="event-description">{event.description}</div>
+        <div className="event-summary">{event.summary}</div>
         <div className="event-time">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M12 2C6.486 2 2 6.486 2 12C2 17.514 6.486 22 12 22C17.514 22 22 17.514 22 12C22 6.486 17.514 2 12 2ZM12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12C20 16.411 16.411 20 12 20Z" fill="white"/>
@@ -36,7 +38,13 @@ class Event extends Component {
           <path d="M12 2C9.87827 2 7.84344 2.84285 6.34315 4.34315C4.84285 5.84344 4 7.87827 4 10C4 11.892 4.402 13.13 5.5 14.5L12 22L18.5 14.5C19.598 13.13 20 11.892 20 10C20 7.87827 19.1571 5.84344 17.6569 4.34315C16.1566 2.84285 14.1217 2 12 2V2Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
           {event.location}</div>
-        <button className="button__show-details" onClick={() => this.expandEvent(event)}>Show details</button>
+        <button className="button__show-details" onClick={() => this.expandEvent(event)}>{collapsed ? "Show details" : "Hide details"}</button>
+        
+        {!collapsed &&
+            <div className={`event-details ${this.state.collapsed ? "hide" : "show"}`}>
+              <div className="event-description">{event.description}</div>
+            </div>
+          }
       </div>
     );
   }
