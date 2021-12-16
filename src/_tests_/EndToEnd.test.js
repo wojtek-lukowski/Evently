b34d6721d6007c30059c42566af2701de0d31389
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-16 13:42:58 +0100

    End-to-end test bonus (not complete)

--- src/_tests_/EndToEnd.test.js
+++ src/_tests_/EndToEnd.test.js
@@ -37,3 +37,41 @@ describe('show/hide an event details', () => {
   })
 
 });
+
+describe('filter events by cities ', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+      browser = await puppeteer.launch({
+      // headless: false,
+      // slowMo: 250,
+      // ignoreDefaultArgs: ['--disable-extensions']
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000/');
+  })
+  
+  afterAll(() => {
+    browser.close();
+  })
+  
+  test('show alll events', async () => {
+    await page.waitForSelector('.EventList');
+    const EventList = await page.$('.EventList');
+    expect(EventList).toBeDefined();
+  })
+
+  test('show list of suggestions', async () => {
+    await page.waitForSelector('.city');
+    await page.type('.city', 'query');
+    await page.waitForSelector('.suggestions');
+    expect('.suggestions').toBeDefined();
+  });
+
+  test('Select a city from the suggestion list', async () => {
+    await page.waitForSelector('.suggestions');
+    await page.click('.suggestions');
+  })
+//not finished
+});
