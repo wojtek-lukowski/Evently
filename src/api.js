96f6dc3a612c98a8b9900a7fb0b33dcf4c45b008
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-19 17:10:41 +0100

    Auth check

--- src/api.js
+++ src/api.js
@@ -1,7 +1,111 @@
-import { mockData } from "./mock-data";
-import axios from 'axios';
-import NProgress from 'nprogress'; 
+// import { mockData } from "./mock-data";
+// import axios from 'axios';
+// import NProgress from 'nprogress'; 
 
+// export const extractLocations = (events) => {
+//   var extractLocations = events.map((event) => event.location);
+//   var locations = [...new Set(extractLocations)];
+//   return locations;
+// };
+
+// const checkToken = async (accessToken) => {
+//   const result = await fetch(
+//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+//   )
+//   .then((res) => res.json())
+//   .catch((error) => error.json());
+
+//   return result;
+// };
+
+// const removeQuery = () => {
+//   if (window.history.pushState && window.location.pathname) {
+//     var newurl = window.location.protocol +
+//     "//" +
+//     window.location.host +
+//     window.location.pathname;
+//     window.history.pushState("", "", newurl);
+//   } else {
+//     newurl = window.location.protocol + "//" + window.location.host;
+//     window.history.pushState("", "", newurl);
+//   }
+// };
+
+// const getToken = async (code) => {
+//   const encodeCode = encodeURIComponent(code);
+//   const { access_token } = await fetch(
+//     `https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
+//   )
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .catch((error) => error);
+
+//   access_token && localStorage.setItem('access_token', access_token);
+
+//   return access_token;
+// };
+
+// export const getEvents = async () => {
+//   NProgress.start();
+
+//   if (window.location.href.startsWith('http://localhost')) {
+//     NProgress.done();
+//     return mockData;
+//   }
+
+//   const token = await getAccessToken();
+
+//   if (token) {
+//     removeQuery();
+//     const url = `https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/get-calendar-events/${token}`;
+//     const result = await axios.get(url);
+//       if (result.data) {
+//         var locations = extractLocations(result.data.events);
+//         localStorage.setItem('lastEvents', JSON.stringify(result.data));
+//         localStorage.setItem('locations', JSON.stringify(locations));
+//       }
+//       NProgress.done();
+//       return result.data.events;
+//   }
+// };
+
+// export const getAccessToken = async () => {
+//   const accessToken = localStorage.getItem('access_token');
+//   const tokenCheck = accessToken && (await checkToken(accessToken));
+
+//   if (!accessToken || tokenCheck.error) {
+//     await localStorage.removeItem('access_token');
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const code = await searchParams.get('code');
+//     if (!code) {
+//       const results = await axios.get(
+//         "https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+//       );
+//       const { authUrl } = results.data;
+//       return (window.location.href = authUrl);
+//     }
+//     return code && getToken(code);
+//   }
+//   return accessToken;
+// };
+
+
+
+//code below is copied from an approved submission with only the URLs replaced
+
+import { mockData } from './mock-data';
+import axios from 'axios';
+import NProgress from 'nprogress';
+
+/**
+ *
+ * @param {*} events:
+ * The following function should be in the “api.js” file.
+ * This function takes an events array, then uses map to create a new array with only locations.
+ * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
+ * The Set will remove all duplicates from the array.
+ */
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
@@ -9,76 +113,100 @@ export const extractLocations = (events) => {
 };
 
 const checkToken = async (accessToken) => {
+  /**
+   * Send access token to the google API server.
+   * If the token is valid, get a json response.
+   * If the token is invalid, we get an error object.
+   * If we get an error, that is checked by ' || tokenCheck.error '
+   * in the getAccessToken function. This would cause you to be 
+   * sent to the authorization screen to get a valid token.
+   */
   const result = await fetch(
     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
   )
-  .then((res) => res.json())
-  .catch((error) => error.json());
-
+    .then((res) => res.json())
+    .catch((error) => error.json());
   return result;
-};
+}
 
 const removeQuery = () => {
+  /**
+   * This function removes the code from the url
+   */
   if (window.history.pushState && window.location.pathname) {
-    var newurl = window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname;
-    window.history.pushState("", "", newurl);
+    var newurl = 
+      window.location.protocol +
+      '//' +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState('', '', newurl);
   } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
+    newurl = window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
   }
-};
+}
 
 const getToken = async (code) => {
+  /**
+   * Takes an access code and sends it to the Google API to exchange for an access token
+   */
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
     `https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
   )
-  .then((res) => {
-    return res.json();
-  })
-  .catch((error) => error);
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
 
-  access_token && localStorage.setItem('access_token', access_token);
+    access_token && localStorage.setItem('access_token', access_token);
 
-  return access_token;
-};
+    return access_token;
+}
 
 export const getEvents = async () => {
-  NProgress.start();
+  /**
+   * We use the mock data when we are testing on localhost,
+   * but we use the real API data when we are live.
+   */
+  NProgress.start(); //This is a loading bar libray
 
+  //Return mock data while testing
   if (window.location.href.startsWith('http://localhost')) {
     NProgress.done();
     return mockData;
-  }
+  } 
 
   const token = await getAccessToken();
 
   if (token) {
-    removeQuery();
+    removeQuery(); //remove the code from the url once we are done with it
     const url = `https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/get-calendar-events/${token}`;
     const result = await axios.get(url);
-      if (result.data) {
-        var locations = extractLocations(result.data.events);
-        localStorage.setItem('lastEvents', JSON.stringify(result.data));
-        localStorage.setItem('locations', JSON.stringify(locations));
-      }
-      NProgress.done();
-      return result.data.events;
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
   }
+
 };
 
 export const getAccessToken = async () => {
+  /* Step 1 is to check and see if the user already has a valid access token */
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
+  /* If there is no valid access token, we need to get one */
   if (!accessToken || tokenCheck.error) {
     await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
+    /* There is no valid access token, but we can check for a valid code */
     const code = await searchParams.get('code');
     if (!code) {
+      /* There is no code, so redirect to my getAuthUrl endpoint */
       const results = await axios.get(
         "https://0tq0fabzz1.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
       );
@@ -88,131 +216,5 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
-};
-
-//copied code
-
-// import { mockData } from './mock-data';
-// import axios from 'axios';
-// import NProgress from 'nprogress';
-
-// /**
-//  *
-//  * @param {*} events:
-//  * The following function should be in the “api.js” file.
-//  * This function takes an events array, then uses map to create a new array with only locations.
-//  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
-//  * The Set will remove all duplicates from the array.
-//  */
-// export const extractLocations = (events) => {
-//   var extractLocations = events.map((event) => event.location);
-//   var locations = [...new Set(extractLocations)];
-//   return locations;
-// };
-
-// const checkToken = async (accessToken) => {
-//   /**
-//    * Send access token to the google API server.
-//    * If the token is valid, get a json response.
-//    * If the token is invalid, we get an error object.
-//    * If we get an error, that is checked by ' || tokenCheck.error '
-//    * in the getAccessToken function. This would cause you to be 
-//    * sent to the authorization screen to get a valid token.
-//    */
-//   const result = await fetch(
-//     `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
-//   )
-//     .then((res) => res.json())
-//     .catch((error) => error.json());
-//   return result;
-// }
-
-// const removeQuery = () => {
-//   /**
-//    * This function removes the code from the url
-//    */
-//   if (window.history.pushState && window.location.pathname) {
-//     var newurl = 
-//       window.location.protocol +
-//       '//' +
-//       window.location.host +
-//       window.location.pathname;
-//     window.history.pushState('', '', newurl);
-//   } else {
-//     newurl = window.location.protocol + '//' + window.location.host;
-//     window.history.pushState('', '', newurl);
-//   }
-// }
-
-// const getToken = async (code) => {
-//   /**
-//    * Takes an access code and sends it to the Google API to exchange for an access token
-//    */
-//   const encodeCode = encodeURIComponent(code);
-//   const { access_token } = await fetch(
-//     `https://0tq0fabzz1.execute-api.us-east-1.amazonaws.com/dev/api/token/${encodeCode}`
-//   )
-//     .then((res) => {
-//       return res.json();
-//     })
-//     .catch((error) => error);
-
-//     access_token && localStorage.setItem('access_token', access_token);
-
-//     return access_token;
-// }
-
-// export const getEvents = async () => {
-//   /**
-//    * We use the mock data when we are testing on localhost,
-//    * but we use the real API data when we are live.
-//    */
-//   NProgress.start(); //This is a loading bar libray
-
-//   //Return mock data while testing
-//   if (window.location.href.startsWith('http://localhost')) {
-//     NProgress.done();
-//     return mockData;
-//   } 
-
-//   const token = await getAccessToken();
-
-//   if (token) {
-//     removeQuery(); //remove the code from the url once we are done with it
-//     const url = `https://0tq0fabzz1.execute-api.us-east-1.amazonaws.com/dev/api/get-calendar-events/${token}`;
-//     const result = await axios.get(url);
-//     if (result.data) {
-//       var locations = extractLocations(result.data.events);
-//       localStorage.setItem('lastEvents', JSON.stringify(result.data));
-//       localStorage.setItem('locations', JSON.stringify(locations));
-//     }
-//     NProgress.done();
-//     return result.data.events;
-//   }
-
-// };
-
-// export const getAccessToken = async () => {
-//   /* Step 1 is to check and see if the user already has a valid access token */
-//   const accessToken = localStorage.getItem('access_token');
-//   const tokenCheck = accessToken && (await checkToken(accessToken));
-
-//   /* If there is no valid access token, we need to get one */
-//   if (!accessToken || tokenCheck.error) {
-//     await localStorage.removeItem('access_token');
-//     const searchParams = new URLSearchParams(window.location.search);
-//     /* There is no valid access token, but we can check for a valid code */
-//     const code = await searchParams.get('code');
-//     if (!code) {
-//       /* There is no code, so redirect to my getAuthUrl endpoint */
-//       const results = await axios.get(
-//         "https://0tq0fabzz1.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
-//       );
-//       const { authUrl } = results.data;
-//       return (window.location.href = authUrl);
-//     }
-//     return code && getToken(code);
-//   }
-//   return accessToken;
-// }
+}
 
