8d1d42c25f5590021d4e52717118ed030e5e4efd
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-20 15:25:24 +0100

    Error Alert added

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 
   render() {
     const { numberOfEvents, setNumber } = this.props;
+    const errorText = 'Number of events to display must be between 1 and 100';
 
     return (
       <div className="NumberOfEvents">
+      { (numberOfEvents < 1 || numberOfEvents > 100) &&
+          <ErrorAlert text={errorText}/>
+      }
       <p className="number-of-events">Show</p>
       <input type="number" min="5" max="50" step="5"
       value={numberOfEvents} id="NumberOfEvents" onChange={(e) => setNumber(e)} 
