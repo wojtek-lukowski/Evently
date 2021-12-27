727319c25e5c403c1729a93fd89f02a638afc823
Wojtek Lukowski <wlukowski@4qu.de>
2021-12-27 13:36:14 +0100

    Google app verification added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
 import Header from './Header';
 import EventGenre from './EventGenre';
+import WelcomeScreen from './WelcomeScreen';
 import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 
@@ -17,19 +18,27 @@ class App extends Component {
     events: [],
     locations: [],
     numberOfEvents: 30,
-    currentLocation: 'all'
+    currentLocation: 'all',
+    showWelcomeScreen: undefined
   }
 
   async componentDidMount() {
     const { numberOfEvents } = this.state;
     this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
     getEvents().then((events) => {
       if (this.mounted) {
         this.setState({
           events: events.slice(0, numberOfEvents),
           locations: extractLocations(events) });
-      }
-    });
+        }
+      });
+    }
   }
 
   componentWillUnmount(){
@@ -66,6 +75,7 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className='App' />
 
     return (
       <div className="App">
@@ -87,6 +97,8 @@ class App extends Component {
             </ResponsiveContainer>
         </div>
         <EventList events={this.state.events}/>
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './WelcomeScreen.css';
+function WelcomeScreen(props) {
+  return props.showWelcomeScreen ?
+    (
+      <div className="WelcomeScreen">
+        <h1>Evently</h1>
+        <h4>All the full-stack events in one place
+        </h4>
+        <div className="button_cont" align="center">
+          <div class="google-btn">
+            <div class="google-icon-wrapper">
+              <img
+                class="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="Google sign-in"
+              />
+            </div>
+            <button onClick={() => { props.getAccessToken() }}
+              rel="nofollow noopener"
+              class="btn-text"
+            >
+              <b>Sign in with Google</b>
+            </button>
+          </div>
+        </div>
+        <a
+          href="https://wojtek-lukowski.github.io/evently/privacy.html"
+          rel="nofollow noopener"
+        >
+          Privacy policy
+        </a>
+      </div>)
+    : null
+}
+export default WelcomeScreen;
